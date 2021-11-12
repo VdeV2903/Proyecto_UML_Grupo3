08554fc56de181776a4cad9b87eb50b4749a2134
MEDINA SALAZAR MARCOS ADONAY <2925592020@mail.utec.edu.sv>
2021-11-12 00:40:56 -0600

    Terminar Reportes y docs fiscales

--- Capa_Diseño/frmEmpleados.Designer.cs
+++ Capa_Diseño/frmEmpleados.Designer.cs
@@ -188,7 +188,7 @@ namespace Capa_Diseño
             this.tabPage2.Location = new System.Drawing.Point(4, 29);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1062, 597);
+            this.tabPage2.Size = new System.Drawing.Size(1064, 597);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Agregar Empleado";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -294,7 +294,7 @@ namespace Capa_Diseño
             this.tabPage3.Location = new System.Drawing.Point(4, 29);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(1062, 597);
+            this.tabPage3.Size = new System.Drawing.Size(1064, 597);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Modificar Empleado";
             this.tabPage3.UseVisualStyleBackColor = true;
